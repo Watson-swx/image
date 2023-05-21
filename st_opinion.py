aa039f3fc41eb8af8411490a182ed37f44e0010c
Watson-swx <63901585+Watson-swx@users.noreply.github.com>
2023-05-21 12:13:44 +0800

    Update st_opinion.py

--- st_opinion.py
+++ st_opinion.py
@@ -1,32 +1,10 @@
 import streamlit as st
+import pandas as pd
 #from HK import synthesis
 #from CPOE import evolution
-import math
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=('第%d列' % (i+1) for i in range(5))
+)
 
-st.title('观点演化模型信息系统')
-
-num = st.number_input('输入一个数字')
-st.write('the square equals ',math.sqrt(num))
-
-'''
-st.sidebar.text('HK model parameter:')
-epsilon1 = st.sidebar.slider('epsilon1', min_value=0.0,max_value=1.0)
-
-st.sidebar.text('CPOE model parameter:')
-epsilon2 = st.sidebar.slider('epsilon2', min_value=0.0,max_value=1.0)
-lambd = st.sidebar.slider('lambda', min_value=0.0,max_value=1.0)
-theta = st.sidebar.slider('theta', min_value=0.0,max_value=1.0)
-
-if st.button('Run opinion evolution model'):
-    col1,col2 = st.columns(2)
-
-    # HK model
-    with col1:
-        filepath = synthesis(epsilon1)
-        st.image(filepath,caption='HK model')
-
-    # CPOE model
-    with col2:
-        filepath = evolution(epsilon2,lambd,theta)
-        st.image(filepath,caption='CPOE model')
-'''
+st.table(df)
