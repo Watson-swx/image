16a0d08ec732142c9691d49a77ea5c2c47ecacc2
Watson-swx <63901585+Watson-swx@users.noreply.github.com>
2023-10-11 00:17:23 +0800

    Add files via upload

---
+++ streamapi.py
@@ -0,0 +1,34 @@
+# chat_bot.py
+ 
+import openai
+import streamlit as st
+from streamlit_chat import message
+ 
+#申请的api_key
+openai.api_key = "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"
+def generate_response(prompt):
+    completion = openai.Completion.create(
+        model="text-davinci-003",  #'gpt-3.5-turbo',
+        prompt=prompt,
+        max_tokens=1024,
+        temperature=0
+    )
+    message=completion.choices[0].text
+    return message
+ 
+st.markdown("#### 我是ChatGPT聊天机器人,我可以回答您的任何问题！")
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("请输入您的问题:",key='input')
+if user_input:
+    output=generate_response(user_input)
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], 
+                is_user=True, 
+                key=str(i)+'_user')
